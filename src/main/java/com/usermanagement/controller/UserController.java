6b63f4a3287c04b9577ed9ce3ff2483f1216b83a
Sandeep Nilla <sandeep.nillaibm.com@sandeeps-air.dfw.ibm.com>
2017-08-11 15:38:14 -0500

    Converted existing application to spring boot application

---
+++ src/main/java/com/usermanagement/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.usermanagement.controller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usermanagement.model.User;
+import com.usermanagement.repository.UserRepository;
+@RestController
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Das Boot, reporting for duty!";
+	}
+	//@GET
+	//@Produces(MediaType.APPLICATION_JSON)
+	@RequestMapping(value="/rest/{userName}/{lastName}", method = RequestMethod.GET)
+	@ResponseBody
+	public User searchUser(@PathVariable String userName,
+			@PathVariable String lastName){
+			return userRepository.findOne((long) 1);
+	}
+	
+//	@POST
+//	@Consumes(MediaType.APPLICATION_JSON)
+	@RequestMapping(value="/rest", method=RequestMethod.POST)
+	public User addUser(@RequestBody User userDtls){
+//		UserDAO userDao=new UserDAO();
+		return userRepository.saveAndFlush(userDtls);
+	}
+}
---
+++ src/main/java/com/usermanagement/model/User.java
@@ -0,0 +1,59 @@
+package com.usermanagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="user_dtls")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+	private String password;
+	private String company;
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	  public String getFirstName() {
+	        return firstName;
+	}
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	}
+	    
+	    public long getId() {
+	        return id;
+	    }
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	  
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+   
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+     
+}
